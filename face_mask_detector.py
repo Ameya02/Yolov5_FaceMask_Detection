219f7ba0840c10a58a313579b2bc7baa9843dfe0
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2022-08-21 16:34:36 +0530

    Changes

--- face_mask_detector.py
+++ face_mask_detector.py
@@ -16,6 +16,7 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 from models.common import DetectMultiBackend
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
+from utils.dataloaders import LoadImages
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device
 
